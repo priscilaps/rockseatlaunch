c15262e581d7eaff7e924f166ac3cf8ca2c0abd0
Priscila <priscilapsampaio@gmail.com>
2020-04-19 19:44:18 -0300

    desafio3-2 concluído

---
+++ modulo03/data.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+        id: "starter",
+        logo: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        title: "Starter",
+        duration: "X Módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        logo: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        title: "Launchbase",
+        duration: "6 semanas",
+        price: "Comprar"
+    },
+    {
+        id: "gostack",
+        logo: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        title: "GoStack",
+        duration: "X Módulos",
+        price: "Comprar",
+        featured: true
+    }
+]
--- modulo03/server.js
+++ modulo03/server.js
@@ -2,21 +2,34 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const cursos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res){
-    return res.render("sobre")
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/62256293?s=400&u=ba02a432034663573e9ceaee2b435e90756a6523&v=4",
+        name: "Priscila Pereira Sampaio",
+        role: "Web and graphic designer, photographer, and craftswoman.",
+        tecnologias: ["HTML","CSS"],
+        links: [
+            { name: "Facebook", url: "https://www.facebook.com/pritchps" },
+            { name: "Instagram", url: "https://www.instagram.com/pritchps" },
+            { name: "Site", url: "https://www.priscilasampaio.com.br" }
+        ]
+    }
+    return res.render("sobre", { about })
 })
 
 server.get("/conteudos", function(req, res){
-    return res.render("conteudos")
+    return res.render("conteudos",{ items: cursos })
 })
 
 server.use(function(req, res) {
