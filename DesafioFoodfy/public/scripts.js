4f0274f5073e232d6459f441fab36156f5bf5954
Priscila <priscilapsampaio@gmail.com>
2020-04-21 21:46:37 -0300

    refatorando o Foodfy concluído finalmente - ajustes finais (remoção dos console.log de teste)

--- DesafioFoodfy/public/scripts.js
+++ DesafioFoodfy/public/scripts.js
@@ -8,35 +8,26 @@ for (const recipe of recipes) {
 }
 
 const wrappers = document.querySelectorAll(".wrapper")  //pega os wrappers de ingredientes, modo de preparo e informações adicionais
-for (wrapper of wrappers){
-    const buttonState = wrapper.querySelector(".button")
-    const contentDisplay = wrapper.querySelector(".button-content")
-
-    console.log(buttonState)
-    console.log(contentDisplay)
+for (wrapper of wrappers){                                              //pra cada wrapper (cada um só tem .button e um .button-content)
+    const buttonState = wrapper.querySelector(".button")                //pega o elemento do botao mostrar/esconder
+    const contentDisplay = wrapper.querySelector(".button-content")     //pega a div que o botao vai esconder/mostrar
 
     buttonState.addEventListener('click', function(){
-    console.log(contentDisplay.classList.contains("show"))
-    console.log(buttonState.textContent)
 
     const classes = contentDisplay.classList
     
         if ( classes.contains("show") ) {
 
-            console.log(`tenho a classe SHOW -> minhas classes: ${{classes}}`)
-            contentDisplay.classList.add('hide')
+            contentDisplay.classList.add("hide")
             contentDisplay.classList.remove("show")
             buttonState.textContent = "mostrar" 
-            console.log(buttonState.textContent)
-            console.log(classes)
     
         }else if (classes.contains("hide")){
-    
-            console.log(`tenho a classe HIDE -> minhas classes:${{classes}}`)
-            
+
             contentDisplay.classList.add("show")
             contentDisplay.classList.remove("hide")
             buttonState.textContent = "esconder"
+
         }
     
         
